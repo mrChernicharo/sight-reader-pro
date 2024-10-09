82d64c2c2e7c7e921393f0682dcab0d999e06017
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-10-09 09:23:10 -0300

    TextInput -> save username

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,18 +1,34 @@
+import AppButton from "@/components/atoms/AppButton";
+import { AppText } from "@/components/atoms/AppText";
 import { AppView } from "@/components/atoms/AppView";
+import { BackLink } from "@/components/atoms/BackLink";
+import { useAppStore } from "@/hooks/useStore";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { ComponentType, useRef, useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function SettingsScreen() {
+  const { username, setUsername } = useAppStore();
+  const [localUsername, setLocalUsername] = useState(username);
+
   return (
     <AppView style={s.container}>
-      <TouchableOpacity>
-        <Link href="/">
-          <Ionicons name="arrow-back" size={24} />
-        </Link>
-      </TouchableOpacity>
-      <Text>Settings</Text>
+      <BackLink />
+
+      <AppText type="title">Settings</AppText>
+
+      <AppText>username</AppText>
+      <AppView style={s.inputContainer}>
+        <TextInput style={s.input} onChangeText={setLocalUsername} defaultValue={username} />
+        <AppButton
+          text="submit"
+          textStyle={{ color: "white" }}
+          activeOpacity={0.7}
+          disabled={!localUsername}
+          onPress={() => localUsername && setUsername(localUsername)}
+        />
+      </AppView>
     </AppView>
   );
 }
@@ -22,5 +38,17 @@ const s = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 36,
+  },
+  inputContainer: {
+    width: "100%",
+    gap: 12,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#999",
+    borderRadius: 4,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
   },
 });
